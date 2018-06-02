aab7d2ff512c878566364a2fcac551b04b237053
Centheos1 <clint@centheos.com>
2018-06-02 13:22:05 +1000

    Added basic menu structure

---
+++ Submission/Word Count/Word Count/CountMenuViewController.swift
@@ -0,0 +1,37 @@
+//
+//  CountMenuViewController.swift
+//  Word Count
+//
+//  Created by Clint Sellen on 2/6/18.
+//  Copyright © 2018 UTS. All rights reserved.
+//
+
+import UIKit
+
+class CountMenuViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    @IBAction func unwindToMain(_ sender: UIButton) {
+    }
+    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
--- Submission/Word Count/Word Count/MainMenuViewController.swift
+++ Submission/Word Count/Word Count/MainMenuViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  MainMenuViewController.swift
 //  Word Count
 //
 //  Created by Clint Sellen on 2/6/18.
@@ -8,13 +8,16 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class MainMenuViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
 
+    @IBAction func unwindToMain(unwindSegue: UIStoryboardSegue) {
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
