7357a2dcd4689e866dd70eab0cff5efaabf458b1
Centheos1 <clint@centheos.com>
2018-06-03 21:00:38 +1000

    started on word game, added background

--- Submission/Word Count/Word Count/WordGameScene.swift
+++ Submission/Word Count/Word Count/WordGameScene.swift
@@ -10,49 +10,15 @@ import SpriteKit
 import GameplayKit
 
 class WordGameScene: SKScene {
-    var multiplierLabel: SKLabelNode!
     var viewWidth = 1024
     var viewHeight = 750
-    //    var entities = [GKEntity]()
-    //    var graphs = [String : GKGraph]()
-    //
-    //    private var lastUpdateTime : TimeInterval = 0
-    //    private var label : SKLabelNode?
-    //    private var spinnyNode : SKShapeNode?
+//    Animiated Background
+    var backgroundSpeed: CGFloat = 200.0 // Speed of background
+    var deltaTime: TimeInterval = 0
+    var lastUpdateTimeInterval: TimeInterval = 0
     
     override func sceneDidLoad() {
-        
-        // Set the score multiplier
-        multiplierLabel = SKLabelNode(fontNamed: "Avenir Next Condensed Bold")
-        multiplierLabel.text = "1.5x"
-        multiplierLabel.horizontalAlignmentMode = .center
-        multiplierLabel.verticalAlignmentMode = .center
-        multiplierLabel.position = CGPoint(x: viewWidth/2, y: Int(Float(viewHeight)*0.8))
-        multiplierLabel.fontSize = 120
-        multiplierLabel.alpha = 1
-        multiplierLabel.zPosition = 1
-        addChild(multiplierLabel)
-        //        self.lastUpdateTime = 0
-        
-        // Get label node from scene and store it for use later
-        //        self.label = self.childNode(withName: "//helloLabel") as? SKLabelNode
-        //        if let label = self.label {
-        //            label.alpha = 0.0
-        //            label.run(SKAction.fadeIn(withDuration: 2.0))
-        //        }
-        
-        // Create shape node to use during mouse interaction
-        //        let w = (self.size.width + self.size.height) * 0.05
-        //        self.spinnyNode = SKShapeNode.init(rectOf: CGSize.init(width: w, height: w), cornerRadius: w * 0.3)
-        //
-        //        if let spinnyNode = self.spinnyNode {
-        //            spinnyNode.lineWidth = 2.5
-        //
-        //            spinnyNode.run(SKAction.repeatForever(SKAction.rotate(byAngle: CGFloat(Double.pi), duration: 1)))
-        //            spinnyNode.run(SKAction.sequence([SKAction.wait(forDuration: 0.5),
-        //                                              SKAction.fadeOut(withDuration: 0.5),
-        //                                              SKAction.removeFromParent()]))
-        //        }
+        setUpBackgrounds()
     }
     
     
@@ -103,20 +69,60 @@ class WordGameScene: SKScene {
     
     override func update(_ currentTime: TimeInterval) {
         // Called before each frame is rendered
+        if lastUpdateTimeInterval == 0 {
+            lastUpdateTimeInterval = currentTime
+        }
         
-        // Initialize _lastUpdateTime if it has not already been
-        //        if (self.lastUpdateTime == 0) {
-        //            self.lastUpdateTime = currentTime
-        //        }
+        deltaTime = currentTime - lastUpdateTimeInterval
+        lastUpdateTimeInterval = currentTime
         
-        // Calculate time since last update
-        //        let dt = currentTime - self.lastUpdateTime
-        //
-        //        // Update entities
-        //        for entity in self.entities {
-        //            entity.update(deltaTime: dt)
-        //        }
-        //
-        //        self.lastUpdateTime = currentTime
+        updateBackground()
     }
+    
+    func setUpBackgrounds() {
+        //add background
+        
+        for i in 0...7 {
+            
+            let background = SKSpriteNode(imageNamed: "\(i).png")
+            background.anchorPoint = CGPoint(x: 0, y: 0)
+            background.position = CGPoint(x: CGFloat(i) * size.width, y: 0)
+            background.size = CGSize(width: self.size.width, height: self.size.height)
+            background.zPosition = -1
+            background.name = "Background"
+            self.addChild(background)
+            
+        }
+        
+        let world = SKSpriteNode(imageNamed: "world")
+        world.anchorPoint = CGPoint(x: 0, y: 0)
+        world.position = CGPoint(x: 25, y: size.height / 4)
+        world.zPosition = 1
+        world.alpha = 0.3
+        world.name = "world"
+        self.addChild(world)
+        
+        let blackOverlay = SKSpriteNode(imageNamed: "black-gradient")
+        blackOverlay.anchorPoint = CGPoint(x: 0, y: 0)
+        blackOverlay.position = CGPoint(x: 0, y: 0)
+        blackOverlay.zPosition = 1
+        blackOverlay.name = "world"
+        self.addChild(blackOverlay)
+    }
+    
+    func updateBackground() {
+        self.enumerateChildNodes(withName: "Background") { (node, stop) in
+            
+            if let back = node as? SKSpriteNode {
+                let move = CGPoint(x: -self.backgroundSpeed * CGFloat(self.deltaTime), y: 0)
+                back.position += move
+                
+                if back.position.x < -back.size.width {
+                    back.position += CGPoint(x: back.size.width * CGFloat(8), y: 0)
+                }
+            }
+            
+        }
+    }
+
 }
